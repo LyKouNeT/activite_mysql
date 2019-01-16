85ce474fb2291f044066bbd10117f99b6b895e5a
LyK <lyk@LyK.LyK>
2019-01-16 22:26:02 +0100

    J'ai réussi à faire une première verison de l'amélioration 3 qui fonctionne dans le fichier req_global/req3.sql J'ai quand même le sentiment que l'on peut faire quelque chose de plus élégant avec peut être une seule requête. A creser !

--- req_global/req3.sql
+++ req_global/req3.sql
@@ -10,64 +10,53 @@ INNER JOIN Utilisateur
 GROUP BY utilisateur_id
 ORDER BY utilisateur_id;
 
--- -- Je cherche à avoir le nombre de commentaire écrit par utilisateur.
--- SELECT Utilisateur.id AS utilisateur_id,
--- --	Article.id AS article_id,
--- 	SUM(VM_article_nbre_commentaire.nombre_commentaires)
--- FROM Utilisateur
--- INNER JOIN Article
--- 	ON Article.auteur_id = Utilisateur.id
--- INNER JOIN VM_article_nbre_commentaire
--- 	ON Article.id = VM_article_nbre_commentaire.article_id
--- -- GROUP BY utilisateur_id, article_id;
--- GROUP BY utilisateur_id;
+-- Je cherche à avoir le nombre de commentaire écrit par utilisateur.
+DROP TABLE IF EXISTS VM_utilisateur_commentaire;
+
+CREATE TABLE VM_utilisateur_commentaire
+SELECT 	Utilisateur.id AS utilisateur_id,
+	COUNT(Commentaire.id) AS nombre_commentaires_ecris,
+	MAX(Commentaire.date_commentaire) AS date_publication_dernier_commentaire
+FROM Commentaire
+INNER JOIN Utilisateur
+	ON Commentaire.auteur_id = Utilisateur.id
+GROUP BY utilisateur_id
+ORDER BY utilisateur_id;
+
+SELECT * FROM VM_utilisateur_commentaire;
+
 
 -- Test où j'essaie d'abord d'avoir toutes 
 -- les informations.
 -- J'ai un doute sur la pertinance du nombre de commentaire
 -- Je dois vérifier cela 
 SELECT Utilisateur.id AS utilisateur_id,
-	Article.id as article_id, -- Pour test
 	Article.date_publication,
 	VM_utilisateur_nbre_article.nombre_article AS nombre_article_ecris,
-	Commentaire.id AS commentaire_id
--- 	Commentaire.date_commentaire
+	VM_utilisateur_commentaire.nombre_commentaires_ecris AS nombre_commentaires_ecris,
+	VM_utilisateur_commentaire.date_publication_dernier_commentaire
 FROM Utilisateur
 INNER JOIN Article
  	ON Utilisateur.id = Article.auteur_id
 INNER JOIN VM_utilisateur_nbre_article
  	ON Utilisateur.id = VM_utilisateur_nbre_article.utilisateur_id
-LEFT OUTER JOIN Commentaire 
- 	ON Utilisateur.id = Commentaire.auteur_id
-ORDER BY utilisateur_id ASC, Article.date_publication DESC, commentaire_id ASC;
+LEFT OUTER JOIN VM_utilisateur_commentaire
+	ON Utilisateur.id = VM_utilisateur_commentaire.utilisateur_id
+ORDER BY utilisateur_id ASC, Article.date_publication DESC;
 
--- -- Test où j'essaie d'avoir la table 
--- -- avec uniquement les infos voulu
+
+-- Cette version fonctionne
 SELECT Utilisateur.id AS utilisateur_id,
-	MAX(Article.date_publication) AS date_dernier_article,
-	VM_utilisateur_nbre_article.nombre_article AS nombre_articles_ecris,
-	COUNT(Commentaire.id) AS nombre_commentaires_ecris,
-	MAX(Commentaire.date_commentaire) AS date_dernier_commentaire
+	MAX(Article.date_publication) AS date_publication_dernier_article,
+	VM_utilisateur_nbre_article.nombre_article AS nombre_article_ecris,
+	VM_utilisateur_commentaire.nombre_commentaires_ecris AS nombre_commentaires_ecris,
+	VM_utilisateur_commentaire.date_publication_dernier_commentaire
 FROM Utilisateur
-INNER JOIN VM_utilisateur_nbre_article
- 	ON Utilisateur.id = VM_utilisateur_nbre_article.utilisateur_id
 INNER JOIN Article
  	ON Utilisateur.id = Article.auteur_id
-LEFT OUTER JOIN Commentaire 
- 	ON Utilisateur.id = Commentaire.auteur_id
-GROUP BY utilisateur_id
-ORDER BY utilisateur_id;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+INNER JOIN VM_utilisateur_nbre_article
+ 	ON Utilisateur.id = VM_utilisateur_nbre_article.utilisateur_id
+LEFT OUTER JOIN VM_utilisateur_commentaire
+	ON Utilisateur.id = VM_utilisateur_commentaire.utilisateur_id
+GROUP BY utilisateur_id, nombre_article_ecris, nombre_commentaires_ecris, date_publication_dernier_commentaire
+ORDER BY utilisateur_id ASC, Article.date_publication DESC;
